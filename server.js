85001ecb3a319c19acc723dfde079dec4d731b9d
abg3000 <abg3000@naver.com>
2021-11-26 22:44:07 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(8080, () => {
+  console.log("listening on 8080");
+});
